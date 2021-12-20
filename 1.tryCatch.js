18c8cebef11a564d1f44cb1945979feac17ddb55
Anna Sargsyan <anna.sargsyan.818830@gmail.com>
2021-12-21 01:26:45 +0400

    if a not a string print error message

---
+++ 1.tryCatch.js
@@ -0,0 +1,16 @@
+"use strict";
+/*Task 1
+Complete the reverseString function, it has one parameter s. You must perform the following actions:
+1. Try to reverse string s using the split, reverse, and join methods.
+2. If an exception is thrown, catch it and print the contents of the exception's message on a new line.
+3. Print s on new line. If no exception was thrown, then this should be the reversed string, if an exception was thrown, this should be the original string.
+*/
+function reverseString(s) {
+  try {
+    s = s.split("").reverse().join("");
+  } catch (e) {
+    return e.message;
+  }
+  return s;
+}
+console.log(reverseString(3456));
